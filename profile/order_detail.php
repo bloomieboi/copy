4cdcfee50a687d2e62c98ed82d5e08d50c2ef2da
bloomieboi <king.andrey2006@gmail.com>
2026-03-03 18:48:40 +0000

    fuck

--- profile/order_detail.php
+++ profile/order_detail.php
@@ -46,7 +46,7 @@ if (!$order) {
 // Получаем адрес заказа (точку обслуживания)
 $stmt = $pdo->prepare("SELECT CONCAT(l.location_name, ' - ', l.address) as address_name 
                        FROM order_address oa 
-                       JOIN locations l ON oa.address_id = l.location_id 
+                       JOIN locations l ON oa.location_id = l.location_id 
                        WHERE oa.order_id = ?");
 $stmt->execute([$orderId]);
 $address = $stmt->fetch();
